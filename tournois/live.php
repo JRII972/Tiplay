6c386e8ebf19350e78a45cdfb1f17f07c3638762
JRII972 <59899019+JRII972@users.noreply.github.com>
2020-04-06 19:34:21 -0400

    stream creat

--- tournois/live.php
+++ tournois/live.php
@@ -13,11 +13,22 @@
     <!-- Bootstrap core CSS -->
 <link href="../res/css/bootstrap.min.css" rel="stylesheet" >
 <link href="../res/css/standar.css" rel="stylesheet">
+<script type="text/javascript" >
+  function initElement()
+{
+  var p = document.getElementById("click");
+  // NOTE: showAlert(); ou showAlert(param); NE fonctionne PAS ici.
+  // Il faut fournir une valeur de type function (nom de fonction déclaré ailleurs ou declaration en ligne de fonction).
+  p.onclick = openStream(GNjStWG2vLU, YT);
+};
 
-  function openStream(videoID, platform)  {
-
-  }
-
+  function openStream(videoID, platform) {
+    var creating = browser.tabs.create({
+      active: 'true',
+      url: './stream.php?id=' + videoId + 'platform=' + platform
+    });
+  };
+</script>
 
     <style>
       .bd-placeholder-img {
@@ -38,7 +49,7 @@
     <!-- Custom styles for this template -->
     <link href="../res/css/product.css" rel="stylesheet">
   </head>
-  <body>
+  <body onload="initElement();">
     <nav class="site-header sticky-top py-1" style="width: 100%;">
       <div class="container navBar">
         <a class="py-2" href="index.html" aria-label="Product">
@@ -77,7 +88,7 @@
       
       
         <div class=" bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-          <div class="my-3 py-3">
+          <div class="my-3 py-3" 4>
             <h2 class="display-5">TIPLAY Parties</h2>
             <p class="lead">Prouvez que vous êtes le meillieur</p>
           </div>
@@ -92,9 +103,22 @@
 Quis Lorem aliqua nulla esse et consequat reprehenderit sunt cillum labore excepteur adipisicing sunt. Nisi excepteur incididunt et aliquip cillum laborum incididunt minim laboris duis fugiat ut officia mollit. Ut veniam ut laboris aute in exercitation qui.        
       </p>
       
-      <form action="./stream.php" method="get"><input type="submit" name="id" value="GNjStWG2vLU"></form>
-        <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <!-- Ici crée une boucle qui vas généré les capsules vers les vidéos -->
+       <?php /*
+          foreach ($xmlPartie->partie as $partie) {
+              $name = $partie->nom;
+              $date = DateTime::createFromFormat(trim($partie->horaire[0]['format']," \t\r\n\x0b"),trim($partie->horaire," \t\r\n\x0b"));
+              $description = $partie->description;
+              $price = (float)$partie->prix;
+              $img = $partie->image;  
+        */ ?>
+
+        <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3" >
+          <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden" onclick="window.open(
+                './stream.php?id=GNjStWG2vLU&platform=YT',
+                '_blank'
+            );
+            " id="click">
             <div class="my-3 p-3">
               <h2 class="display-5">Baima</h2>
               <p class="lead">Bientôt</p>
@@ -104,32 +128,25 @@ Quis Lorem aliqua nulla esse et consequat reprehenderit sunt cillum labore excep
           </div>
           <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div class="my-3 p-3">
-              <h2 class="display-5">Comming soon</h2>
+              <h2 class="display-5">Learn To loose</h2>
               <p class="lead">Bientôt</p>
             </div>
             <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0; background-image: url('../res/img/coming soon.jpg') ; 
           background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
+          </div>
+         
+          <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+              <h2 class="display-5">Comming soon</h2>
+              <p class="lead">Bientôt</p>
+            </div>
+            <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0; background-image: url('./res/img/coming soon.jpg') ; 
+            background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
           </div>
         </div>
       
-        <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div class="my-3 p-3">
-              <h2 class="display-5">Comming soon</h2>
-              <p class="lead">Bientôt</p>
-            </div>
-            <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0; background-image: url('../res/img/coming soon.jpg') ; 
-          background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
-          </div>
-          <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div class="my-3 p-3">
-              <h2 class="display-5">Comming soon</h2>
-              <p class="lead">Bientôt</p>
-            </div>
-            <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0; background-image: url('../res/img/coming soon.jpg') ; 
-          background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
-          </div>
-        </div>
+          <?php /*
+          } */?>
     </div>
   </body>
 <footer class="container py-5">
